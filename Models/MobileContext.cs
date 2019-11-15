2a1aa17c2287f7df815aceac88dd9966e6fed043
jaymehta-12 <56089842+jaymehta-12@users.noreply.github.com>
2019-11-15 14:31:18 +0530

    Add files via upload

---
+++ Models/MobileContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileStore.Models
+{
+    public class MobileContext :DbContext
+    {
+        public MobileContext(DbContextOptions<MobileContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MobileItems> MobileItems { get; set; }
+
+        public DbSet<AccessoryItems> AccessoryItems { get; set; }
+    }
+}
