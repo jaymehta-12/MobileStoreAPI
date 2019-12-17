ea348dea959ec9a8820fe1dfa9ebf4c8be423b78
jaymehta-12 <56089842+jaymehta-12@users.noreply.github.com>
2019-12-17 21:12:33 +0530

    Add files via upload

---
+++ Models/MobileItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileStore.Models
+{
+    public class MobileItems
+    {
+
+        public int MobileItemsId { get; set; }
+
+        public string MobileName { get; set; }
+
+        public int MobilePrice { get; set; }
+
+        public List<AccessoryItems> AccessoryItems { get; set; }
+    }
+
+    
+
+}
